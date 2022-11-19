b0ccb40239a9a1c37fdc49bb645dfa29af5c5e9f
Suraj <surajaskmini@gmail.com>
2022-11-19 19:08:18 +0545

    Day 1- Learning FastAPI Scratch

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
+from typing import Optional
+
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app=FastAPI()
+
+
+class Post(BaseModel):
+    title:str
+    content:str
+    published:bool=True
+    rating:Optional[int]=None
+
 
 @app.get("/")
 async def root():
     return{"message":"Hello world"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data":"This is your posts"}
+
+@app.post("/createposts")
+def create_post(body:Post):
+    print(body)
+    print(body.dict())
+    return {"new post":body};
